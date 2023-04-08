8e28e4148bf88276c3061db8dfd2a42cb4983ac2
Bedo Hossam <bedohossam404@gmail.com>
2023-04-08 19:13:44 +0200

    first commit

---
+++ src/Components/Content.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Content(props) {
+  const item = props.item;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleLeftClick = () => {
+    let newIndex = currentImageIndex - 1;
+    if (newIndex < 0) {
+      newIndex = item.images.length - 1;
+    }
+    setCurrentImageIndex(newIndex);
+  };
+
+  const handleRightClick = () => {
+    let newIndex = currentImageIndex + 1;
+    if (newIndex >= item.images.length) {
+      newIndex = 0;
+    }
+    setCurrentImageIndex(newIndex);
+  };
+
+  return (
+    <div className="Content">
+      <h1>{item.name}</h1>
+      <div className="media">
+        <span className="arrow left" onClick={handleLeftClick}>
+          &larr;
+        </span>
+        <img alt="ok" src={item.images[currentImageIndex]} />
+        <span className="arrow right" onClick={handleRightClick}>
+          &rarr;
+        </span>
+      </div>
+      <section className="compare">
+        <p>Company:&nbsp;&nbsp; {item.Company} </p>
+        <p>Genre :&nbsp;&nbsp; {item.Genre} </p>
+        <p>Price :&nbsp;&nbsp; {item.Price} </p>
+      </section>
+    </div>
+  );
+}
---
+++ src/Components/Nav.js
@@ -0,0 +1,14 @@
+
+ import { FaDiscord, FaGithub, FaLinkedin } from "react-icons/fa";
+
+
+export default function Icons() {
+  return (
+    <div className="icons">
+      <a href="https://github.com/bedohossam22" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+      <a href="https://discord.gg/GhYbC54M" target="_blank" rel="noopener noreferrer"><FaDiscord /></a>
+      <a href="https://www.linkedin.com/in/bedo-hossam-5a9286247/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+    </div>
+  );
+}
+ 
